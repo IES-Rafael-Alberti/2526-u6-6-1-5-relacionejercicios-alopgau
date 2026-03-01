69d278d8dc6175f6f8552ba75740ffe16d01ea5d
alopgau <alopezg@ieslabahia.es>
2026-03-01 13:20:31 +0100

    Ejemplo extensión añadida 2º subclase

--- src/main/kotlin/6.1.kt
+++ src/main/kotlin/6.1.kt
@@ -18,11 +18,28 @@ class Movil(val marca: String, val modelo: String, val plataformaDeAudio: String
 // Extensión
 
 open class Login() {
-    open fun pedirCredenciales() = println("Introduce tu nombre de usuario")
+    open fun pedirCredenciales() {
+        println("Introduce tu nombre de usuario")
+        readln()
+    }
 }
 class LoginConCuenta : Login() {
     override fun pedirCredenciales() {
         super.pedirCredenciales()
         println("Introduce tu contraseña")
+        readln()
+    }
+}
+class LoginConCuenta2FA : Login() {
+    override fun pedirCredenciales() {
+        var codigoValido = false
+      val usuario = super.pedirCredenciales()
+        println("Introduce tu contraseña")
+        readln()
+        while (!codigoValido) {
+        println("Introduce el codigo numérico de 6 digitos que hemos enviado al correo asociado a tu usuario ($usuario)")
+        val codigo = readln()
+        if (codigo.length == 6 && codigo.all { it.isDigit() }) codigoValido = true else println("El código introducido no es válido")
+        }
     }
 }
