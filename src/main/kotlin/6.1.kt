2ed6307fa03cd9926c055336a1f68d91d80f5be0
alopgau <alopezg@ieslabahia.es>
2026-03-01 12:49:35 +0100

    Clases y 1º propuesta

---
+++ src/main/kotlin/6.1.kt
@@ -0,0 +1,28 @@
+package ej6_1
+
+// Especificación
+
+interface ReproductorAudio {
+    fun reproducirAudio(): Unit
+}
+class MP3(val marca: String, val modelo: String) : ReproductorAudio {
+    override fun reproducirAudio() = println("Mi MP3 $marca $modelo hace sonar mis canciones favoritas")
+}
+class Timbre() : ReproductorAudio {
+    override fun reproducirAudio() = println("El timbre hace ding dong")
+}
+class Movil(val marca: String, val modelo: String, val plataformaDeAudio: String): ReproductorAudio {
+    override fun reproducirAudio() = println("Escucho música en mi $marca $modelo a través de $plataformaDeAudio")
+}
+
+// Extensión
+
+open class Login() {
+    open fun pedirCredenciales() = println("Introduce tu nombre de usuario")
+}
+class LoginConCuenta : Login() {
+    override fun pedirCredenciales() {
+        super.pedirCredenciales()
+        println("Introduce tu contraseña")
+    }
+}
