e83926fce3849f1a5323b6cbb77da3adf3b63629
alopgau <alopezg@ieslabahia.es>
2026-03-06 13:10:58 +0100

    DIP hecho y teoria ISP

---
+++ src/main/kotlin/es/ies/ejercicios/u6/ej65/dip/DipNuevo.kt
@@ -0,0 +1,27 @@
+package ej6_1.es.ies.ejercicios.u6.ej65.dip
+
+import ej6_1.es.ies.ejercicios.u6.ej65.ocp.nuevo.Informe
+import ej6_1.es.ies.ejercicios.u6.ej65.ocp.nuevo.Csv
+import es.ies.ejercicios.u6.ej64.Persona
+import es.ies.ejercicios.u6.ej64.Resumible
+
+
+class ControladorInformesV0(private val generador: Informe) {
+
+
+    fun imprimirListado(items: List<Resumible>) {
+        val salida = generador.generar("Listado DIP", items)
+        println(salida)
+    }
+}
+
+fun main() {
+    val controller = ControladorInformesV0(Csv())
+    controller.imprimirListado(
+        listOf(
+            Persona("Ana", 20),
+            Persona("Luis", 19),
+        ),
+    )
+}
+
